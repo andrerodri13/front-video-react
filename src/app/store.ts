c80e54a9bd824def6863a29e41a5c8a7e99c0afc
André S. Rodrigues <andrerodri13@hotmail.com>
2023-01-09 18:57:01 -0300

    Criação dos hooks da api

--- src/app/store.ts
+++ src/app/store.ts
@@ -2,6 +2,7 @@ import {configureStore, ThunkAction, Action} from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
 import categoriesReducer, {categoriesApiSlice} from "../features/categories/categorySlice";
 import {apiSlice} from "../features/api/apiSlice";
+import {castMembersApiSlice} from "../features/cast/castMembersSlice";
 
 export const store = configureStore({
     reducer: {
@@ -9,6 +10,7 @@ export const store = configureStore({
         categories: categoriesReducer,
         [apiSlice.reducerPath]: apiSlice.reducer,
         [categoriesApiSlice.reducerPath]: apiSlice.reducer,
+        [castMembersApiSlice.reducerPath]: apiSlice.reducer,
     },
     middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware().concat(apiSlice.middleware),
---
+++ src/types/CastMembers.ts
@@ -0,0 +1,44 @@
+export interface Results {
+    data: CastMember[];
+    links: Links;
+    meta: Meta;
+}
+
+export interface Result {
+    data: CastMember;
+    links: Links;
+    meta: Meta;
+}
+
+export interface CastMember {
+    id: string;
+    name: string;
+    type: number;
+    deletedAt: null;
+    createdAt: string;
+    updatedAt: string;
+}
+
+export interface Links {
+    first: string;
+    last: string;
+    prev: null;
+    next: string;
+}
+
+export interface Meta {
+    to: number;
+    from: number;
+    path: string;
+    total: number;
+    perPage: number;
+    lastPage: number;
+    currentPage: number;
+}
+
+export interface CastMemberParams {
+    page?: number;
+    perPage?: number;
+    search?: string;
+    type?: number;
+}
